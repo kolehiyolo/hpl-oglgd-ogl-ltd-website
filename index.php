5059a6966fb0331c77000a7fcbdf107789f13645
kolehiyolo <tristanseanpaul.cinco@gmail.com>
2022-10-12 00:52:39 +0800

    Setup Project Breakdown
    1. I setup the project directory and have everything organized
    2. What I did was have each page's styling broken down by section
    3. This way, when we begin development, we will only be working on each section's file, ensuring that the rest is unaffected
    4. This will make working much less underwhelming because we will cleraly only think of each chunk at a time and also ensuring that css-targetting/won't be as difficult as before
    5. I've also setup the SASS compiler to have the exports be in the correct css directory
    6. Overall, the project just looks real pristine
    7. I'm thinking of optimizing this further by also dividing the PHP into modules, but that's a tale for another day

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="sass/css/home.css">
+    <link rel="stylesheet" href="css/home.css">
     <title>OGL Ltd | Home</title>
 </head>
 
@@ -14,48 +14,90 @@
     </header>
 
     <main class="main">
-        <div class="section">
-            <div class="box-1">
-                <div class="content">
-                    <h2>Box 1</h2>
-                    <p>text1</p>
-                    <p>text2</p>
-                    <p>text3</p>
+        <section class="main-hero">
+            <h2>Hero</h2>
+        </section>
+        <section class="main-lore">
+            <h2>Lore</h2>
+        </section>
+        <section class="main-connector">
+            <h2>Connector</h2>
+        </section>
+        <section class="main-opportunity">
+            <h2>Opportunity</h2>
+        </section>
+        <section class="main-features">
+            <h2>Features</h2>
+        </section>
+        <section class="main-media">
+            <div class="main-media-head">
+                <h2>Media Mentions</h2>
+            </div>
+            <div class="main-media-body">
+                
+            </div>
+        </section>
+        <section class="main-roadmap">
+            <h2>Roadmap</h2>
+            <div class="main-roadmap-graphic">
+                <div class="box-1">
+                    <div class="content">
+                        <h2>Box 1</h2>
+                        <p>text1</p>
+                        <p>text2</p>
+                        <p>text3</p>
+                    </div>
+                </div>
+                <div class="box-2">
+                    <div class="content">
+                        <h2>Box 2</h2>
+                        <p>text1</p>
+                        <p>text2</p>
+                        <p>text3</p>
+                    </div>
+                </div>
+                <div class="box-3">
+                    <div class="content">
+                        <h2>Box 3</h2>
+                        <p>text1</p>
+                        <p>text2</p>
+                        <p>text3</p>
+                    </div>
+                </div>
+                <div class="box-4">
+                    <div class="content">
+                        <h2>Box 4</h2>
+                        <p>text1</p>
+                        <p>text2</p>
+                        <p>text3</p>
+                    </div>
+                </div>
+                <div class="box-5">
+                    <div class="content">
+                        <h2>Box 5</h2>
+                        <p>text1</p>
+                        <p>text2</p>
+                        <p>text3</p>
+                    </div>
                 </div>
             </div>
-            <div class="box-2">
-                <div class="content">
-                    <h2>Box 2</h2>
-                    <p>text1</p>
-                    <p>text2</p>
-                    <p>text3</p>
-                </div>
-            </div>
-            <div class="box-3">
-                <div class="content">
-                    <h2>Box 3</h2>
-                    <p>text1</p>
-                    <p>text2</p>
-                    <p>text3</p>
-                </div>
-            </div>
-            <div class="box-4">
-                <div class="content">
-                    <h2>Box 4</h2>
-                    <p>text1</p>
-                    <p>text2</p>
-                    <p>text3</p>
-                </div>
-            </div>
-            <div class="box-5">
-                <div class="content">
-                    <h2>Box 5</h2>
-                    <p>text1</p>
-                    <p>text2</p>
-                    <p>text3</p>
-                </div>
-            </div>
-        </div>
+        </section>
+        <section class="main-team">
+            <h1>Team</h1>
+        </section>
+        <section class="main-advisors">
+            <h1>Advisors</h1>
+        </section>
+        <section class="main-bts">
+            <h1>Behind the Scenes</h1>
+        </section>
+        <section class="main-partners">
+            <h1>Partners</h1>
+        </section>
+        <section class="main-pre_footer">
+            <h1>Pre-footer</h1>
+        </section>
+
     </main>
 
     <footer class="footer">
