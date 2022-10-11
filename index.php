e7af02fb157526082d36fc47bb604f22cc9195dd
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2022-10-11 21:42:29 +0800

    Initial Push
    1. Continuing work later

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sass/css/home.css">
+    <title>OGL Ltd | Home</title>
+</head>
+
+<body>
+    <header class="header">
+    </header>
+
+    <main class="main">
+        <div class="section">
+            <div class="box-1">
+                <div class="content">
+                    <h2>Box 1</h2>
+                    <p>text1</p>
+                    <p>text2</p>
+                    <p>text3</p>
+                </div>
+            </div>
+            <div class="box-2">
+                <div class="content">
+                    <h2>Box 2</h2>
+                    <p>text1</p>
+                    <p>text2</p>
+                    <p>text3</p>
+                </div>
+            </div>
+            <div class="box-3">
+                <div class="content">
+                    <h2>Box 3</h2>
+                    <p>text1</p>
+                    <p>text2</p>
+                    <p>text3</p>
+                </div>
+            </div>
+            <div class="box-4">
+                <div class="content">
+                    <h2>Box 4</h2>
+                    <p>text1</p>
+                    <p>text2</p>
+                    <p>text3</p>
+                </div>
+            </div>
+            <div class="box-5">
+                <div class="content">
+                    <h2>Box 5</h2>
+                    <p>text1</p>
+                    <p>text2</p>
+                    <p>text3</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+    </footer>
+</body>
+
+</html>
