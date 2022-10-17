527a92de1d97b0cedbeb24c6411f1899e7971f40
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2022-10-17 15:22:32 +0800

    Remove the HTML Builder Functionality so Lods can work on the raw HTML

--- javascript/pages/home/team-carousel.js
+++ javascript/pages/home/team-carousel.js
@@ -1,45 +1,46 @@
-function buildHTML() {
-    let carouselHTML = ``;
-    let classPrefix = `main-team-body-carousel-item`;
+// function buildHTML() {
+//     let carouselHTML = ``;
+//     let classPrefix = `main-team-body-carousel-item`;
 
-    teamMembers.forEach((item) => {
-        let itemHTML = `<div class="${classPrefix}">`;
-        itemHTML += `<div class="${classPrefix}-inner">`;
+//     teamMembers.forEach((item) => {
+//         let itemHTML = `<div class="${classPrefix}">`;
+//         itemHTML += `<div class="${classPrefix}-inner">`;
 
-        // * Front Side
-        itemHTML += `<div class="${classPrefix}-inner-front">`;
-        itemHTML += `<img src="${item.img}" alt="">`;
-        itemHTML += `<div class="${classPrefix}-inner-front-text">`;
-        itemHTML += `<h3>${item.name}</h3>`;
-        itemHTML += `<p>${item.position}</p>`;
-        itemHTML += `</div>`;
-        itemHTML += `</div>`;
+//         // * Front Side
+//         itemHTML += `<div class="${classPrefix}-inner-front">`;
+//         itemHTML += `<img src="${item.img}" alt="">`;
+//         itemHTML += `<div class="${classPrefix}-inner-front-text">`;
+//         itemHTML += `<h3>${item.name}</h3>`;
+//         itemHTML += `<p>${item.position}</p>`;
+//         itemHTML += `</div>`;
+//         itemHTML += `</div>`;
 
-        // * Back Side
-        itemHTML += `<div class="${classPrefix}-inner-back">`;
-        itemHTML += `<div class="${classPrefix}-inner-back-head">`;
-        itemHTML += `<h3>${item.name}</h3>`;
-        itemHTML += `<p>${item.position}</p>`;
-        itemHTML += `</div>`;
-        itemHTML += `<div class="${classPrefix}-inner-back-text">`;
-        item.description.forEach((desc) => {
-            itemHTML += `<p>${desc}</p>`;
-        });
-        itemHTML += `</div>`;
-        itemHTML += `</div>`;
+//         // * Back Side
+//         itemHTML += `<div class="${classPrefix}-inner-back">`;
+//         itemHTML += `<div class="${classPrefix}-inner-back-head">`;
+//         itemHTML += `<h3>${item.name}</h3>`;
+//         itemHTML += `<p>${item.position}</p>`;
+//         itemHTML += `</div>`;
+//         itemHTML += `<div class="${classPrefix}-inner-back-text">`;
+//         item.description.forEach((desc) => {
+//             itemHTML += `<p>${desc}</p>`;
+//         });
+//         itemHTML += `</div>`;
+//         itemHTML += `</div>`;
 
-        itemHTML += `</div>`;
-        itemHTML += `</div>`;
+//         itemHTML += `</div>`;
+//         itemHTML += `</div>`;
 
-        carouselHTML += itemHTML;
-    });
+//         carouselHTML += itemHTML;
+//     });
 
-    $(`.main-team-body-carousel`).html(carouselHTML);
-}
+//     $(`.main-team-body-carousel`).html(carouselHTML);
+// }
+// buildHTML();
 
 function setupOwlCarousel() {
     let $owl = $('.owl-carousel');
-
+    
     $(document).ready(function () {
         $owl.owlCarousel({
             loop: true,
@@ -50,16 +51,15 @@ function setupOwlCarousel() {
             startPosition: 0,
         });
     });
-
+    
     $owl.children().each(function (index) {
         $(this).attr('data-position', index);
     });
-
+    
     $(document).on('click', '.owl-item>div', function () {
         let $speed = 300;
         $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
     });
 }
 
-buildHTML();
 setupOwlCarousel();
--- pages/home/team.php
+++ pages/home/team.php
@@ -4,6 +4,127 @@
     </div>
     <div class="main-team-body">
         <div class="main-team-body-carousel owl-carousel">
+            <div class="main-team-body-carousel-item">
+                <div class="main-team-body-carousel-item-inner">
+                    <div class="main-team-body-carousel-item-inner-front"><img src="https://imgur.com/ROKhflx.jpg" alt="">
+                        <div class="main-team-body-carousel-item-inner-front-text">
+                            <h3>Daniel Saito</h3>
+                            <p>CEO and Co-founder</p>
+                        </div>
+                    </div>
+                    <div class="main-team-body-carousel-item-inner-back">
+                        <div class="main-team-body-carousel-item-inner-back-head">
+                            <h3>Daniel Saito</h3>
+                            <p>CEO and Co-founder</p>
+                        </div>
+                        <div class="main-team-body-carousel-item-inner-back-text">
+                            <p>Daniel was behind the MySQL database exit to Sun Microsystems for$1B+. He also architected SEGA.NET, the first peer-to-peer gaming ISP on the SEGA Dreamcast and more recently founded Original Gamer Life in Tokyo, Japan in 2021.</p>
+                            <p>He has contributed to security/encryption and MPEG4 on 4G mobile. Separately he structured global nodes for THETA, SETI, and Folding@home, while working for Fortune 100 companies the last 20+ years.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-team-body-carousel-item">
+                <div class="main-team-body-carousel-item-inner">
+                    <div class="main-team-body-carousel-item-inner-front"><img src="https://imgur.com/ROKhflx.jpg" alt="">
+                        <div class="main-team-body-carousel-item-inner-front-text">
+                            <h3>Colin Charles</h3>
+                            <p>Chief Technology Officer</p>
+                        </div>
+                    </div>
+                    <div class="main-team-body-carousel-item-inner-back">
+                        <div class="main-team-body-carousel-item-inner-back-head">
+                            <h3>Colin Charles</h3>
+                            <p>Chief Technology Officer</p>
+                        </div>
+                        <div class="main-team-body-carousel-item-inner-back-text">
+                            <p>Colin Charles is a Consultant at Codership, the makers of Galera Cluster.</p>
+                            <p>Previously, Colin was on the founding team of MariaDB Server, and has been around the MySQL ecosystem including being an early employee at MySQL, and worked actively on the Fedora andOpenOffice.org projects. Colin has been a MySQL user since 2000.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-team-body-carousel-item">
+                <div class="main-team-body-carousel-item-inner">
+                    <div class="main-team-body-carousel-item-inner-front"><img src="https://imgur.com/ROKhflx.jpg" alt="">
+                        <div class="main-team-body-carousel-item-inner-front-text">
+                            <h3>Ben Way</h3>
+                            <p>VP Fundraising</p>
+                        </div>
+                    </div>
+                    <div class="main-team-body-carousel-item-inner-back">
+                        <div class="main-team-body-carousel-item-inner-back-head">
+                            <h3>Ben Way</h3>
+                            <p>VP Fundraising</p>
+                        </div>
+                        <div class="main-team-body-carousel-item-inner-back-text">
+                            <p>Ben is a best selling author, leading futurologist, technologist, inventor, and entrepreneur. With over twenty years of experience in technology and innovation, Ben has travelled the world seeking out and developing new technologies.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-team-body-carousel-item">
+                <div class="main-team-body-carousel-item-inner">
+                    <div class="main-team-body-carousel-item-inner-front"><img src="https://imgur.com/ROKhflx.jpg" alt="">
+                        <div class="main-team-body-carousel-item-inner-front-text">
+                            <h3>Carlos Bott</h3>
+                            <p>Lead Game Developer</p>
+                        </div>
+                    </div>
+                    <div class="main-team-body-carousel-item-inner-back">
+                        <div class="main-team-body-carousel-item-inner-back-head">
+                            <h3>Carlos Bott</h3>
+                            <p>Lead Game Developer</p>
+                        </div>
+                        <div class="main-team-body-carousel-item-inner-back-text">
+                            <p>Carlos Bott is an experienced game developer with a 35 year history releasing games: Babylon 5: Starship Commander, MLB: Ballpark, &amp; MLB: AtBat.</p>
+                            <p>He's the founder of Triluminary Design Studios, &amp; Deputy Head of the Games Production at De Monfort University in Leicester, UK.</p>
+                            <p>He consults in Metaverse gaming &amp; AI with MLB, Lowe's, Target, and Walmart.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-team-body-carousel-item">
+                <div class="main-team-body-carousel-item-inner">
+                    <div class="main-team-body-carousel-item-inner-front"><img src="https://imgur.com/ROKhflx.jpg" alt="">
+                        <div class="main-team-body-carousel-item-inner-front-text">
+                            <h3>Lucas Wilson</h3>
+                            <p>Operations</p>
+                        </div>
+                    </div>
+                    <div class="main-team-body-carousel-item-inner-back">
+                        <div class="main-team-body-carousel-item-inner-back-head">
+                            <h3>Lucas Wilson</h3>
+                            <p>Operations</p>
+                        </div>
+                        <div class="main-team-body-carousel-item-inner-back-text">
+                            <p>Lucas Wilson is a US Marine Corps veteran &amp; lifelong gamer with over 20 years of combined experience in IT &amp; Security, which now includes blockchain technology &amp; metaverse game development.</p>
+                            <p>He founded MilitaryGamers.com 15 years ago, which supports 5,000 veterans and counting.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="main-team-body-carousel-item">
+                <div class="main-team-body-carousel-item-inner">
+                    <div class="main-team-body-carousel-item-inner-front"><img src="https://imgur.com/ROKhflx.jpg" alt="">
+                        <div class="main-team-body-carousel-item-inner-front-text">
+                            <h3>Natalia Sokolova</h3>
+                            <p>Investor Relations</p>
+                        </div>
+                    </div>
+                    <div class="main-team-body-carousel-item-inner-back">
+                        <div class="main-team-body-carousel-item-inner-back-head">
+                            <h3>Natalia Sokolova</h3>
+                            <p>Investor Relations</p>
+                        </div>
+                        <div class="main-team-body-carousel-item-inner-back-text">
+                            <p>Natalia Sokolova brings over 20 years of global entrepreneurship, strategic consultancy and investor expertise.</p>
+                            <p>She is a 2nd generation officer of a Swiss Family Office. Natalia helped many startups and brands to achieve successful exits.</p>
+                            <p>She is a known speaker and a thought leader. She spent a decade leading Investor Relation for publicly traded US and Canadian firms globally.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
@@ -12,5 +133,5 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
 
-<script src="javascript/pages/home/team.js"></script>
+<!-- <script src="javascript/pages/home/team.js"></script> -->
 <script src="javascript/pages/home/team-carousel.js"></script>
